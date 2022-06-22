e0926a6c75167bb9d54ad7c1b3432dd417d6b3dc
Ondřej Maceška <ondrasias@gmail.com>
2022-06-22 12:19:09 +0200

    Create minimumWaitingTime.py

---
+++ minimumWaitingTime.py
@@ -0,0 +1,22 @@
+# Author : Ondřej Maceška 
+# Date : 22.6.2022
+# Task : https://www.algoexpert.io/questions/minimum-waiting-time
+
+# O(n logn) time, O(1) space (only the space given)
+ 
+def minimumWaitingTime(queries):
+    """ returns the minimum waiting time of queries
+        
+    Args:
+        array (int[ ]) : given queries
+
+    Returns:
+        int: the minimum waiting time of queries
+    """
+    queries.sort()
+    waiting_time = 0
+    total_waiting_time = 0
+    for i in range(1, len(queries)):
+        waiting_time = waiting_time + queries[i - 1]
+        total_waiting_time += waiting_time
+    return total_waiting_time
