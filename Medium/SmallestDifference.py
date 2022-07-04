6d594a4b4b3b14327159f47715d4468446c491e2
Jasasul2 <ondrasias@gmail.com>
2022-07-04 11:08:20 +0200

    Added SmallestDifference assignment

--- Medium/SmallestDifference.py
+++ Medium/SmallestDifference.py
@@ -4,15 +4,24 @@
 
 # O(n * log(n) + m * log(m)) time, O(1) space, 
 # where n is the length of the first array and m is the length of the second array
-def smallest_difference(arrayOne, arrayTwo):
-    arrayOne.sort()
-    arrayTwo.sort()
+def smallest_difference(array_one, array_two):
+    """ Finds the smallest difference between 
+        one int from array_one and one int from array_two
+    Args:
+        array_one (int[ ]) : given array 1
+        array_two (int[ ]) : given array 2
+
+    Returns:
+        int[] : two values, on from each array, having the smallest difference
+    """
+    array_one.sort()
+    array_two.sort()
     i, j = 0, 0
-    min1, min2 = arrayOne[0], arrayTwo[0]
+    min1, min2 = array_one[0], array_two[0]
     smallest_diff = abs(min1 - min2)
-    while(i < len(arrayOne) and j < len(arrayTwo)):
-        num1 = arrayOne[i]
-        num2 = arrayTwo[j]
+    while(i < len(array_one) and j < len(array_two)):
+        num1 = array_one[i]
+        num2 = array_two[j]
         diff = abs(num1 - num2)
         if(diff < smallest_diff):
             smallest_diff = diff
